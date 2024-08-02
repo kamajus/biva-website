5913aa843257c7957559a56d0e37e5e1c1d3ea58
Carlos Mateus <carlosmatateumateus@gmail.com>
2024-08-02 20:52:31 +0200

    style: Apply some eslint rules on Header and Footer components

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,7 @@ export default function Footer() {
   return (
     <footer className="fixed bottom-0 left-0 w-full border-t p-8 text-xs bg-white">
       <div className="flex justify-center items-center">
-        <p>
-          Todos direitos reservados.
-        </p>
+        <p>Todos direitos reservados.</p>
       </div>
     </footer>
   )
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
-import { createClient } from "@/utils/supabase/server"
-import AuthButton from "./AuthButton"
+import { createClient } from '@/utils/supabase/server'
+
+import AuthButton from './AuthButton'
 
 export default function Header() {
   const canInitSupabaseClient = () => {
@@ -16,7 +17,7 @@ export default function Header() {
   return (
     <nav className="fixed top-0 left-0 w-full border-b border-b-foreground/10 h-16 bg-white z-50">
       <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm mx-auto">
-        <h1 className='text-base font-medium'>Beeva</h1>
+        <h1 className="text-base font-medium">Beeva</h1>
         {isSupabaseConnected && <AuthButton />}
       </div>
     </nav>
