4d43d1b76cf3e2b640856081bece7b01bcef986b
Carlos Mateus <carlosmatateumateus@gmail.com>
2024-08-02 19:22:10 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Beeva website
---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import { GeistSans } from 'geist/font/sans'
+
+import './globals.css'
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3000'
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: 'Beeva website',
+  description: 'Oi!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className={GeistSans.className}>
+      <body className="bg-background text-foreground">
+        <main className="min-h-screen flex flex-col items-center">
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
