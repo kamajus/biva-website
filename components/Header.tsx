18796576731ec01bc62014b9459c1d5ca7782f9b
Carlos M. <carlosmatateumateus@gmail.com>
2025-05-25 17:21:25 +0200

    Add app icon to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import { createClient } from '@/utils/supabase/server'
 
 import AuthButton from './AuthButton'
@@ -17,7 +19,16 @@ export default function Header() {
   return (
     <nav className="w-full flex justify-center items-center border-b h-[10vh] bg-white">
       <div className="w-[95%] flex justify-between items-center text-sm">
-        <h1 className="text-base font-medium">Kasa ao</h1>
+        <div className="flex items-center gap-2">
+          <Image
+            src="/assets/icon.png"
+            alt="Kasa ao"
+            width={32}
+            height={32}
+            className="rounded-lg"
+          />
+          <h1 className="text-base font-medium">Kasa ao</h1>
+        </div>
         {isSupabaseConnected && <AuthButton />}
       </div>
     </nav>
